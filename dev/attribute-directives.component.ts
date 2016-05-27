09e82c38cb17e3e2a377e6c7c605b693757f9d9a
Jason Holmberg <jason.holmberg@hci.utah.edu>
2016-05-27 06:09:09 -0600

    attribute directives

--- dev/attribute-directives.component.ts
+++ dev/attribute-directives.component.ts
@@ -5,11 +5,11 @@ import {HighlightDirective} from "./highlight.directive";
 @Component({
     selector: 'my-attribute-directives',
     template: `
-        <div myHighlight>
+        <div [myHighlight]="'red'">
             Highlight me
         </div>
         <br><br>
-        <div myHighlight>
+        <div [myHighlight]="'blue'">
             Another highlight
         </div>
     `,
--- dev/highlight.directive.ts
+++ dev/highlight.directive.ts
@@ -4,19 +4,38 @@ import {Directive, ElementRef, OnInit, Renderer} from "angular2/core";
  */
 
 @Directive({
-    selector: '[myHighlight]'
+    selector: '[myHighlight]',
+    inputs: ['highlightColor:myHighlight'],
+    // acccess things connected to our host element
+    host: {
+        '(mouseenter)': 'onMouseEnter()',
+        '(mouseleave)': 'onMouseLeave()'
+    }
 })
-export class HighlightDirective implements OnInit {
+export class HighlightDirective  {
     private _defaultColor = 'green';
     private _elRef: ElementRef;
     private _renderer: Renderer;
+    highlightColor: string;
 
     constructor(elRef: ElementRef, renderer: Renderer) {
         this._elRef = elRef;
         this._renderer = renderer;
     }
 
-    ngOnInit():any {
-        this._renderer.setElementStyle(this._elRef.nativeElement,'background-color',this._defaultColor);
+    // ngOnInit():any {
+    //     this._renderer.setElementStyle(this._elRef.nativeElement,'background-color', this.highlightColor || this._defaultColor);
+    // }
+
+    onMouseEnter() {
+        this.highlight(this.highlightColor || this._defaultColor);
+    }
+
+    onMouseLeave() {
+        this.highlight(null);
+    }
+
+    private highlight(color: string) {
+        this._renderer.setElementStyle(this._elRef.nativeElement,'background-color', color);
     }
 }
