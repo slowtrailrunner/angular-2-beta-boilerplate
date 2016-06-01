4827cb5408debf9f9e720800d21e2fee835a180d
Jason Holmberg <jason.holmberg@hci.utah.edu>
2016-05-31 21:18:12 -0600

    adds form build with custom greater than zero validator

--- dev/shopping-list/shopping-list-new-item.component.ts
+++ dev/shopping-list/shopping-list-new-item.component.ts
@@ -1,6 +1,7 @@
-import {Component} from "angular2/core";
+import {Component, OnInit} from "angular2/core";
 import {ListItem} from "../list-item";
 import {ShoppingListService} from "./shopping-list.service";
+import {ControlGroup, FormBuilder, Validators, Control} from "angular2/common";
 /**
  * Created by jasonholmberg on 5/27/16.
  */
@@ -8,25 +9,45 @@ import {ShoppingListService} from "./shopping-list.service";
 @Component({
     selector: 'shopping-list-new-item',
     template: `
+      <form [ngFormModel]="myForm" (ngSubmit)="onSubmit()">
         <div class="input">
             <label for="item-name">Name</label>
-            <input type="text" id="item-name" [(ngModel)]="item.name">
+            <input type="text" id="item-name" [(ngModel)]="item.name" [ngFormControl]="myForm.controls['itemName']">
         </div>
         <div class="input">
             <label for="item-amt">Amount</label>
-            <input type="text" id="item-amt" [(ngModel)]="item.amount">
+            <input type="text" id="item-amt" [(ngModel)]="item.amount" [ngFormControl]="myForm.controls['itemAmount']">
         </div>
-        <button (click)="onClick()" [disabled]="item.amount === 0">Add Item</button>
+        <button type="submit" [disabled]="!myForm.valid">Add Item</button>
+      </form>
     `
 })
-export class ShoppingListNewItemComponent {
+export class ShoppingListNewItemComponent implements OnInit{
     item = {name: '', amount: 0};
+    myForm : ControlGroup;
 
-    constructor(private _shippingListService: ShoppingListService) {
+    constructor(private _shippingListService: ShoppingListService,
+                private _formBuilder: FormBuilder) {
 
     }
 
-    onClick() {
+    onSubmit() {
         this._shippingListService.insertItem({name: this.item.name, amount: this.item.amount});
+    }
+
+    ngOnInit():any {
+        this.myForm = this._formBuilder.group({
+            'itemName': ['',Validators.required],
+            'itemAmount': ['',Validators.compose([
+                Validators.required,
+                greaterThanZero
+            ])]
+        })
+    }
+}
+
+function greaterThanZero(control: Control):{[s: string]: boolean} {
+    if (control.value <= 0) {
+        return {isZero: true};
     }
 }
