8dbd82a22579e9221d7dfb82aecdde839e312bdf
Jason Holmberg <jason.holmberg@hci.utah.edu>
2016-06-07 05:50:30 -0600

    imperative routing

--- dev/component2.component.ts
+++ dev/component2.component.ts
@@ -1,8 +1,17 @@
 import {Component} from "angular2/core";
+import {Router} from "angular2/router";
 
 @Component({
-    template: `Component2`
+    template: `
+        <h2>Component2</h2>
+        <button (click)="onNavigate()">Take me to Component1</button>
+    `
 })
 export class Component2 {
 
+    constructor(private _router: Router) {}
+
+    onNavigate() {
+        this._router.navigate(['Component1'])
+    }
 }
